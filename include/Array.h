341cac07f4478e1c4f1e342cb24568568d87536a
Stephan van Veen <vanveen.stephan@gmail.com>
2019-02-22 20:32:24 +0100

    Provide a std::initializer_list constructor for Array

--- include/Array.h
+++ include/Array.h
@@ -1,6 +1,7 @@
 #ifndef MULTI_DIM_CONTAINER_ARRAY
 #define MULTI_DIM_CONTAINER_ARRAY
 
+#include <algorithm>
 #include <array>
 
 namespace md {
@@ -12,6 +13,15 @@ template <class T, size_t N1, size_t ...N>
 class Array<T, N1, N...>
         : public std::array<Array<T, N...>, N1>
 {
+    using Base = std::array<Array<T, N...>, N1>;
+    using Type = typename Base::value_type;
+public:
+    Array() = default;
+
+    Array(std::initializer_list<Type> init)
+    {
+        std::move(std::begin(init), std::end(init), std::begin(*this));
+    }
 
 };
 
@@ -19,6 +29,14 @@ template <class T, size_t N1>
 class Array<T, N1>
         : public std::array<T, N1>
 {
+    using Base = std::array<T, N1>;
+public:
+    Array() = default;
+
+    Array(std::initializer_list<T> init)
+    {
+        std::move(std::begin(init), std::end(init), std::begin(*this));
+    }
 
 };
 
