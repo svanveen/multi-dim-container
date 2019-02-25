b6feb9a8cde05938a7caaa172bb689f91f813601
Stephan van Veen <vanveen.stephan@gmail.com>
2019-02-25 21:42:59 +0100

    Setup CMake files and testing environment

---
+++ test/ArrayTest.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include <Array.h>
+
+using namespace testing;
+
+TEST (ArrayTest, InitEmpty)
+{
+}
+
+TEST (ArrayTest, Init)
+{
+}
+
+TEST (ArrayTest, Fill)
+{
+}
+
+TEST (ArrayTest, At)
+{
+}
+
+TEST (ArrayTest, ConstAt)
+{
+}
+
+TEST (ArrayTest, AccessOperator)
+{
+}
+
+TEST (ArrayTest, ConstAccessOperator)
+{
+}
+
+TEST (ArrayTest, TotalSize)
+{
+}
+
+TEST (ArrayTest, Data)
+{
+}
