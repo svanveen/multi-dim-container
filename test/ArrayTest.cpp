27d598682dee87ee066bf5560ca63435f88bc765
Stephan van Veen <vanveen.stephan@gmail.com>
2019-03-01 17:46:45 +0100

    Implement tests for access

--- test/ArrayTest.cpp
+++ test/ArrayTest.cpp
@@ -17,6 +17,34 @@ TEST (ArrayTest, Fill)
 
 TEST (ArrayTest, At)
 {
+    md::Array<int, 3> arr1i;
+
+    arr1i.at(0) = 1;
+    arr1i.at(1) = 2;
+    arr1i.at(2) = 3;
+
+    ASSERT_TRUE(arr1i == (md::Array<int, 3>{1, 2, 3}));
+
+    md::Array<int, 2, 4> arr2i;
+
+    // dimension wise
+    arr2i.at(0).at(0) = 1;
+    arr2i.at(1).at(0) = 2;
+
+    // pair
+    arr2i.at(std::pair(0, 1)) = 3;
+    arr2i.at(std::pair(1, 1)) = 4;
+
+    // tuple
+    arr2i.at(std::tuple(0, 2)) = 5;
+    arr2i.at(std::tuple(1, 2)) = 6;
+
+    // array
+    arr2i.at(std::array{0, 3}) = 7;
+    arr2i.at(std::array{1, 3}) = 8;
+
+    ASSERT_TRUE(arr2i == (md::Array<int, 2, 4>{{1, 3, 5, 7},
+                                               {2, 4, 6, 8}}));
 }
 
 TEST (ArrayTest, ConstAt)
@@ -52,6 +80,34 @@ TEST (ArrayTest, ConstAt)
 
 TEST (ArrayTest, AccessOperator)
 {
+    md::Array<int, 3> arr1i;
+
+    arr1i[0] = 1;
+    arr1i[1] = 2;
+    arr1i[2] = 3;
+
+    ASSERT_TRUE(arr1i == (md::Array<int, 3>{1, 2, 3}));
+
+    md::Array<int, 2, 4> arr2i;
+
+    // dimension wise
+    arr2i[0][0] = 1;
+    arr2i[1][0] = 2;
+
+    // pair
+    arr2i[std::pair(0, 1)] = 3;
+    arr2i[std::pair(1, 1)] = 4;
+
+    // tuple
+    arr2i[std::tuple(0, 2)] = 5;
+    arr2i[std::tuple(1, 2)] = 6;
+
+    // array
+    arr2i[std::array{0, 3}] = 7;
+    arr2i[std::array{1, 3}] = 8;
+
+    ASSERT_TRUE(arr2i == (md::Array<int, 2, 4>{{1, 3, 5, 7},
+                                               {2, 4, 6, 8}}));
 }
 
 TEST (ArrayTest, ConstAccessOperator)
